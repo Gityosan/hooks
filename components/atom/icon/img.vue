1fb8777ce577539541485588468fa31797e7f15b
Gityosan <ymo6190@docomo.ne.jp>
2023-07-24 09:03:11 +0900

    [feature] add component

---
+++ components/atom/icon/img.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { S3ObjectInput } from '~/assets/API'
+const { $getImage } = useNuxtApp()
+const props = withDefaults(
+  defineProps<{
+    src?: string | null
+    file?: S3ObjectInput | null
+    width?: string
+  }>(),
+  {
+    width: 'auto'
+  }
+)
+const imageUrl = ref<string>(props.src || '')
+onMounted(async () => {
+  if (props.file?.key && props.file?.identityId)
+    imageUrl.value = await $getImage(props.file.key, props.file.identityId)
+})
+</script>
+<template>
+  <v-img
+    :src="imageUrl"
+    :width="width"
+    :aspect-ratio="1 / 1"
+    cover
+    style="aspect-ratio: 1"
+    class="rounded-pill"
+  />
+</template>
---
+++ components/atom/icon/tooltip.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    text: string
+    icon: string
+    color: string
+    size: string | number
+    class: string
+    style: object
+  }>(),
+  {
+    text: '',
+    icon: '',
+    color: 'text-black',
+    size: '24',
+    class: 'ma-2',
+    style: () => ({})
+  }
+)
+</script>
+<template>
+  <v-tooltip :text="text">
+    <template #activator="{ props: tooltip }">
+      <v-icon
+        v-bind="tooltip"
+        :icon="icon"
+        :size="size"
+        :class="[color, props.class]"
+        :style="style"
+        @click="$emit('click')"
+      />
+    </template>
+  </v-tooltip>
+</template>
